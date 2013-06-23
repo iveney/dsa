66abf789d7cbf7be6188c55594fe83d6553e6c33
Zigang Xiao <zxiao@illinois.edu>
2013-06-23 16:39:56 -0700

    Add visualization function

---
+++ visualize.m
@@ -0,0 +1,14 @@
+function visualize(layout)
+%VISUALIZE Visualize a given matrix of dsa layout
+mymap = [
+1 1 0;    % 0 = dummy wire, yellow
+0 0 1;    % 1 = real wire, blue
+1 0 0     % 2 = cut, red
+];
+imagesc(layout);
+colormap(mymap);
+axis image;
+labels = {'dummy','real','cut'};
+lcolorbar(labels,'fontweight','bold');
+end
+
